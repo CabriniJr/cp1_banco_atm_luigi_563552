ce59007315554e768df4c5133b6d89695ff92d80
CabriniJr <luigi.mendes.cabrini@outlook.com>
2026-04-02 12:38:56 -0300

    feat: cadastro e classe de usuario

--- src/FiapBankAtm.java
+++ src/FiapBankAtm.java
@@ -1,14 +1,29 @@
-import java.util.HashMap;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class FiapBankAtm {
-    HashMap<String, String> cadastro = new HashMap<>();
-    HashMap<String, String> usuario = new HashMap<>();
+    ArrayList<Usuario> usuarios = new ArrayList<>();
+
+    Usuario sessao_atual;
+
+    public class Usuario{
+        public String nome;
+        public String senha;
+        double saldo;
+
+        Usuario(String nome, String senha){
+            this.nome = nome;
+            this.senha = senha;
+            this.saldo = 0.0;
+            usuarios.add(this);
+        }
+
+    }
 
 
 
 
-    public static void main(String[] args){
+    public void main(String[] args){
         try{
             Scanner leitor = new Scanner(System.in);
             System.out.println(Mensagens.LOGO.getMensagem());
@@ -21,10 +36,18 @@ public class FiapBankAtm {
                         opcao = login();
                         break;
                     case 2:
-                        opcao = cadastro(leitor);
+                        opcao = cadastro();
+                        break;
+                    case 3:
+                        opcao = sessao();
                         break;
                     case 0:
                         System.exit(0);
+
+                    default:
+                        System.out.println(Mensagens.OP_INICIO.getMensagem());
+                        opcao = leitor.nextInt();
+
                 }
 
             }
@@ -36,14 +59,55 @@ public class FiapBankAtm {
 
     public static int login(){
 
+        return 3;
     }
 
-    public static int cadastro(Scanner leitor){
+    public int cadastro(){
         try{
+            Scanner leitor = new Scanner(System.in);
+            String nome, senha;
 
-                System.out.println(Mensagens.CADASTRO_NOME.getMensagem());
+            System.out.println(Mensagens.CADASTRO_NOME.getMensagem());
+            nome = leitor.nextLine();
+            nome = nome.trim();
 
+            if(!teste_nome(nome)) return 404;
+            //Testa o nome
+
+            System.out.println(Mensagens.CADASTRO_SENHA.getMensagem());
+            senha = leitor.nextLine();
+
+            if(!teste_senha(senha)) return 404;
+            //Testa a senha
+            //Código só chega aqui caso tudo tenha sido validado, logo insiro na lista de usuarios e na sessão atual
+            System.out.println("Sucesso " + nome);
+            sessao_atual = new Usuario(nome,senha);
+
+        }catch (Exception e){
+            return 404;
         }
+        return 3;
+    }
+
+    public static boolean teste_nome(String nome){
+        // Testes executados no nome no cadastro
+        if (nome == null || nome.isBlank()) {
+            System.out.println("Nome inválido.");
+            return false;
+        } else if (!nome.matches("[a-zA-ZÀ-ú ]+")) {
+            System.out.println("Nome deve conter só letras.");
+            return false;
+        }
+        return true;
+    }
+
+    public static boolean teste_senha(String senha){
+        // Teste executados na senha
+        if(!senha.matches("^(?=.*[0-9])(?=.*[A-Z])(?=.*[!@#$%^&*()\\-_+=?><]).{8,}$")){
+            System.out.println("Senha inválida!");
+            return false;
+        }
+        return true;
     }
 
 
@@ -80,6 +144,14 @@ public class FiapBankAtm {
                 """),
         CADASTRO_NOME("""
                 Digite um nome válido
+                """),
+        CADASTRO_SENHA("""
+                Digite sua senha - requisitos
+                - No mínimo 8 caracteres.
+                - Ao menos um número.
+                - Ao menos uma letra maiúscula.
+                - Ao menos um caracter especial da lista: !@#$%^&*()-_+=?><
+                ->
                 """);
 
         private final String Mensagem;
